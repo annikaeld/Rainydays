5f5769250a70bb8a8d574ef71d6e45602789fb2f
annikaeld <130365506+annikaeld@users.noreply.github.com>
2024-03-17 22:38:09 +0100

    Build innerHTML for dynamic elements in jacket-specific.html using JavaSrcipt

---
+++ js/jacket-specific.js
@@ -0,0 +1,13 @@
+function insertProductImage(color) {
+  const productSpecificJacket = document.querySelector(
+    ".product-specific--jacket"
+  );
+  console.log("productSpecificJacket: " + productSpecificJacket);
+
+  productSpecificJacket.innerHTML = `
+      <img src="images/jacketes/jacket-${color}.jpg" alt="${color} Jacket" class="product-image">
+      `;
+  console.log("innerHTML is now:" + productSpecificJacket.innerHTML);
+}
+
+insertProductImage("dusty-rose");
