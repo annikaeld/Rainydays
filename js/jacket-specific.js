85e68c5ad6c1bc9c18f3a422ac116acc67d0f9b5
annikaeld <130365506+annikaeld@users.noreply.github.com>
2024-03-19 13:00:15 +0100

    jacket-specific relativly done

--- js/jacket-specific.js
+++ js/jacket-specific.js
@@ -5,10 +5,44 @@ function insertProductImage(color) {
   console.log("productSpecificJacket: " + productSpecificJacket);
 
   productSpecificJacket.innerHTML = `
-      <img src="images/jacketes/jacket-${color}.jpg" alt="${color} Jacket" class="product-image">
+      <img src=${product.image.url}>
       `;
   console.log("innerHTML is now:" + productSpecificJacket.innerHTML);
 }
 
-insertProductImage("dusty-rose");
-console.log(product.id);
+function insertProductText(text) {
+  const productSpecificDetails = document.querySelector(
+    ".product-specific__details"
+  );
+  console.log("productSpecificDetails: " + productSpecificDetails);
+
+  productSpecificDetails.innerHTML = `<h3>${product.title}</h3>
+  <p class="description-header">Description:</p>
+  <p class="description-paragraph">
+    ${product.description}
+  </p>
+  <a href="check-out.html" class="cta">Add to cart</a>`;
+  console.log("innerHTML is now:" + productSpecificDetails.innerHTML);
+}
+
+function getIdQueryParameter() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const idParam = urlParams.get("id");
+  console.log(idParam);
+  return idParam;
+}
+
+async function productFromApi(id) {
+  let url = `https://v2.api.noroff.dev/rainy-days/${id}`;
+  const response = await fetch(url);
+  const jsonResponse = await response.json();
+  let product = jsonResponse.data;
+  console.log(product);
+  return product;
+}
+
+const id = getIdQueryParameter();
+const product = await productFromApi(id);
+console.log(product);
+insertProductImage();
+insertProductText();
