47710e586f136c10e41b5776e5b7e46097c9c5fe
annikaeld <130365506+annikaeld@users.noreply.github.com>
2024-03-19 14:55:23 +0100

    jackets displaying on sites for men and women according to gender set in API

--- js/women.js
+++ js/women.js
@@ -2,7 +2,7 @@ const url = "https://v2.api.noroff.dev/rainy-days/";
 
 const productList = document.querySelector(".product-list");
 
-async function jacketsMen() {
+async function jacketsWomen() {
   const response = await fetch(url);
 
   const jsonResponse = await response.json();
@@ -13,18 +13,18 @@ async function jacketsMen() {
   for (let i = 0; i < data.length; i++) {
     console.log(data[i].gender);
     let product = data[i];
-    if (product.gender === "Male") {
+    if (product.gender === "Female") {
       console.log(product.id);
 
       productList.innerHTML += `<div class="product">
      <a href="jacket-specific.html?id=${product.id}">
        <img src="${product.image.url}" alt="${product.image.alt}">
        <p>${product.title}</p>
-       <p>${product.price}</p>
+       <p>${product.price}kr</p>
      </a>
    </div>`;
     }
   }
 }
 
-await jacketsMen();
+await jacketsWomen();
